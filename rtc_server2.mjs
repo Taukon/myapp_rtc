8056f3837965cd5cf2f7f9a305c3c5169218642c
Taukon <83957880+Taukon@users.noreply.github.com>
2022-10-01 21:09:21 +0900

    first commit

---
+++ rtc_server2.mjs
@@ -0,0 +1,419 @@
+'use strict';
+
+// --- ES modules ---
+import nutjs from "@nut-tree/nut-js";
+const { mouse, Point, Button, keyboard, Key, screen, ColorMode } = nutjs;
+import Jimp from "jimp";
+
+import { createSocket } from "dgram";
+
+import screenshot from 'screenshot-desktop';
+import https from 'httpolyglot';
+import fs from 'fs';
+import express from 'express';
+import mediasoup from 'mediasoup';
+import { networkInterfaces } from "os";
+//
+// --- commonJS ---
+/*
+const { mouse, Point, Button, keyboard, Key } = require("@nut-tree/nut-js");
+const screenshot = require('screenshot-desktop');
+const https = require('httpolyglot');
+const fs = require('fs');
+const express = require('express');
+const mediasoup = require('mediasoup');
+const { networkInterfaces } = require("os");
+*/
+//
+
+/**
+ * Worker
+ * |-> Router
+ *     |-> WebRtcTransport(s) for Producer
+ *         |-> Producer
+ *     |-> WebRtcTransport(s) for Consumer
+ *         |-> Consumer
+ *     |-> DirectTransport for Producer
+ *         |-> Producer
+ *     |-> DirectTransport(s) for Consumer
+ *         |-> Consumer
+ **/
+
+const MinPort = 2000;   // --- RtcMinPort
+const MaxPort = 2010;   // --- RtcMaxport
+
+const port = 3000;  // --- https Port
+
+let producerList = {};  // --- key: Producer-WebRtcTransport.id, value: transport
+let consumerList = {};  // --- key: Consumer-WebRtcTransport.id, value: transport
+
+let directConsumerList = {};    // --- key: Producer-WebRtcTransport.id, value: transport
+let direcrtDataProducerId;
+
+let sockList = {};  // --- key: Websocket Id, value: {"producerId": transport.id, "consumerId": transport.id}
+let sockIdList = []; // --- Websocket Id
+
+const limitClient = 2;
+
+const interval = 150;//300;
+let predataURL; // --- Screen Image jpeg to base64
+let dataURL;    // --- Screen Image jpeg to base64
+
+// --- HTTPS Server ---
+const app = express();
+
+app.use(express.static('public'));
+
+// --- SSL cert for HTTPS access ---
+const options = {
+    key: fs.readFileSync('./server/ssl/key.pem', 'utf-8'),
+    cert: fs.readFileSync('./server/ssl/cert.pem', 'utf-8')
+}
+
+function getIpAddress() {
+    const nets = networkInterfaces();
+    const net = nets["eth0"]?.find((v) => v.family == "IPv4");
+    return net.address;
+}
+
+const ip_addr = getIpAddress(); // --- IP Address
+
+const httpsServer = https.createServer(options, app)
+httpsServer.listen(port, () => {
+    console.log('listening on port: ' + port);
+    console.log('https://' + ip_addr + ':' + port + '/rtc_client2.html');
+})
+
+// --- WebSocket Server ---
+//const io = require('socket.io')(httpsServer);
+import { Server } from 'socket.io';
+const io = new Server(httpsServer);
+
+io.on('connection', sock => {
+
+    sockList[sock.id] = {};
+    sockIdList.push(sock.id);
+
+    sock.on('getRtpCapabilities', (_, callback) => {
+        //console.log("ListTotal: " + Object.keys(producerList).length);
+        console.log("Total List: " + Object.keys(sockList).length);
+        if (Object.keys(sockList).length > limitClient) {
+            //console.log("sockIdList length: " + sockIdList.length);
+            io.to(sockIdList[0]).emit("end");
+        }
+
+        callback(router.rtpCapabilities);
+    });
+
+    // ----- Producer WebRtcTransport-----
+
+    sock.on('createProducerTransport', async (_, callback) => {
+        const { transport, params } = await mycreateWebRtcTransport();
+        transport.observer.on('close', () => {
+            transport.producer.close();
+            transport.producer = null;
+            delete producerList[transport.id];
+            transport = null;
+        });
+        callback(params);
+
+        producerList[transport.id] = transport;
+        sockList[sock.id]["producerId"] = transport.id;
+    });
+
+    sock.on('connectProducerTransport', async (req, callback) => {
+        const transport = producerList[req.transportId];
+        await transport.connect({ dtlsParameters: req.dtlsParameters });
+        callback({});
+    });
+
+    sock.on('produceData', async (req, callback) => {
+        const transport = producerList[req.transportId];
+        const dataProducer = await transport.produceData(req.produceParameters);
+        callback(dataProducer.id);
+      
+        //console.log("dataProducer.id: " + dataProducer.id);
+        transport.producer = dataProducer;
+
+        //directconsume
+        createDirectConsumer(dataProducer.id, transport.id);
+    });
+
+
+    // ----- Consumer WebRtcTransport -----
+
+    sock.on('createConsumerTransport', async (_, callback) => {
+        const { transport, params } = await mycreateWebRtcTransport();
+        transport.observer.on('close', () => {
+            transport.consumer.close();
+            transport.consumer = null;
+            delete consumerList[transport.id];
+            transport = null;
+        });
+        callback(params);
+
+        consumerList[transport.id] = transport;
+        sockList[sock.id]["consumerId"] = transport.id;
+    });
+
+    sock.on('connectConsumerTransport', async (req, callback) => {
+        const transport = consumerList[req.transportId];
+        await transport.connect({ dtlsParameters: req.dtlsParameters });
+        callback({});
+    });
+
+    // --- use direcrtDataProducerId
+    sock.on('consumeData', async (req, callback) => {
+        const transport = consumerList[req.transportId];
+        const dataConsumer = await transport.consumeData({ dataProducerId: direcrtDataProducerId, });
+        const params = {
+            id: dataConsumer.id,
+            dataProducerId: dataConsumer.dataProducerId,
+            sctpStreamParameters: dataConsumer.sctpStreamParameters,
+            label: dataConsumer.label,
+            protocol: dataConsumer.protocol,
+        };
+        callback(params);
+
+        transport.consumer = dataConsumer;
+
+        //console.log("consumerList.length: " + Object.keys(consumerList).length);
+    });
+
+
+    sock.on("disconnect", () => {
+        console.log("discconect id: " + sock.id);
+        //console.log(JSON.stringify(sockList[sock.id]));
+
+        const producerId = sockList[sock.id]["producerId"];
+        const consumerId = sockList[sock.id]["consumerId"];
+
+        const directTransport = directConsumerList[producerId];
+        console.log("delete directConsumerTransportId: " + directTransport.id);
+        directTransport.close();
+        delete directConsumerList[producerId];
+
+        const sendTransport = producerList[producerId];
+        console.log("delete producerTransportId: " + sendTransport.id);
+        sendTransport.close();
+        delete producerList[producerId];
+
+        const recvTransport = consumerList[consumerId];
+        console.log("delete consumerTransportId: " + recvTransport.id);
+        recvTransport.close();
+        delete consumerList[consumerId];
+
+        delete sockList[sock.id];
+        const indexId = sockIdList.indexOf(sock.id);
+        sockIdList.splice(indexId, 1);
+
+        console.log("delete sockIdList length: " + sockIdList.length);
+    });
+});
+
+
+// --- MediaSoup Server ---
+
+let worker = null;
+let router = null;
+
+const transportOption = {
+    listenIps: [
+        {
+            ip: ip_addr
+        },
+    ],
+    enableSctp: true,
+};
+
+const workerOption = {
+    rtcMinPort: MinPort,
+    rtcMaxPort: MaxPort,
+}
+
+async function startWorker() {
+    worker = await mediasoup.createWorker(workerOption);
+    router = await worker.createRouter({});
+    createDirectProducer();
+}
+
+async function mycreateWebRtcTransport() {
+    const transport = await router.createWebRtcTransport(transportOption);
+    return {
+        transport: transport,
+        params: {
+            id: transport.id,
+            iceParameters: transport.iceParameters,
+            iceCandidates: transport.iceCandidates,
+            dtlsParameters: transport.dtlsParameters,
+            sctpParameters: transport.sctpParameters,
+        }
+    };
+}
+
+// --- Producer DirectTransport ---
+
+async function createDirectProducer() {
+    const transport = await router.createDirectTransport();
+    const dataProducer = await transport.produceData();
+
+    direcrtDataProducerId = dataProducer.id;
+
+    //console.log("direcrtDataProducerId: " + direcrtDataProducerId);
+    
+    setInterval(() => {
+        let image = screen.grab();
+        Jimp.read(image.data).then(image => {
+            console.log("aaaa");
+        })
+        /*
+        screenshot().then((img) => {
+            dataURL = 'data:image/jpeg;base64,' + img.toString('base64');
+            if (dataURL !== predataURL) {
+                predataURL = dataURL;
+                dataProducer.send(dataURL);
+            }
+        });*/
+    }, interval);
+    
+}
+
+// --- Consumer DirectTransport ---
+async function createDirectConsumer(dataProducerId, transportId) {
+    //console.log("createDirectConsumer");
+    const transport = await router.createDirectTransport();
+    directConsumerList[transportId] = transport;
+
+    const dataConsumer = await transport.consumeData({ dataProducerId: dataProducerId });
+    transport.consumer = dataConsumer;
+
+    dataConsumer.on("message", msg => {
+        const data = JSON.parse(msg.toString("utf-8"));
+        //console.log(data);
+
+// ----------- Work nut-js ------------------
+        if(data.move != undefined){
+            try {
+                mymoveMouse(data.move.x, data.move.y);
+                //console.log("try: "+data.move.x +" :"+ data.move.y);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        else if(data.leftclick != undefined){
+            try {
+                if (data.leftclick == "down"){
+                    mouse.pressButton(Button.LEFT);
+                } else if (data.leftclick == "up"){
+                    mouse.releaseButton(Button.LEFT);
+                }
+                //console.log("try leftclick: " + data.leftclick);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        else if (data.rightclick != undefined){
+            try {
+                mymouseClick(Button.RIGHT);
+                //console.log("try rightclick: " + data.rightclick);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        if (data.wheel != undefined) {
+            try {
+                if(data.wheel > 0){
+                    mouse.scrollDown(data.wheel);
+                }else{
+                    mouse.scrollUp(-1 * data.wheel);
+                }
+                //console.log("wheel: "+ data.wheel);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        else if(data.key != undefined){
+            try {
+                if (data.key.symbol != undefined){
+                    mykeyType(data.key.key, data.key.updown);
+                } else{
+                    mykeyToggle(data.key.key, data.key.updown, data.key.mod);
+                }
+                //console.log("try key: " + JSON.stringify(data.key));
+
+            } catch (error) {
+                console.error(error);
+            }
+        }
+// ---------------------------------------------
+    });
+}
+
+async function mymoveMouse(x, y){
+    const target = new Point(x, y);
+    await mouse.setPosition(target);
+}
+
+async function mymouseClick(button) {
+    await mouse.pressButton(button);
+    mouse.releaseButton(button);
+}
+
+async function mykeyToggle(key, updown, mod){
+    if(key != undefined ){
+        mod.unshift(key);
+    }
+
+    if(updown == "down"){
+        await mykeyPress(mod);
+    }else if(updown == "up"){
+        await mykeyRelease(mod);
+    }
+}
+
+async function mykeyPress(key) {
+
+    //console.log("down: "+JSON.stringify(key));
+    switch (key.length) {
+        case 1:
+            await keyboard.pressKey(Key[key[0]]);
+            break;
+        case 2:
+            await keyboard.pressKey(Key[key[1]], Key[key[0]]);
+            break;
+        case 3:
+            await keyboard.pressKey(Key[key[2]], Key[key[1]], Key[key[0]]);
+            break;
+        case 4:
+            await keyboard.pressKey(Key[key[3]], Key[key[2]], Key[key[1]], Key[key[0]]);
+            break;
+    }
+}
+
+async function mykeyRelease(key) {
+
+    //console.log("up: " + JSON.stringify(key));
+    switch(key.length){
+        case 1:
+            await keyboard.releaseKey(Key[key[0]]);
+            break;
+        case 2:
+            await keyboard.releaseKey(Key[key[1]], Key[key[0]]);
+            break;
+        case 3:
+            await keyboard.releaseKey(Key[key[2]], Key[key[1]], Key[key[0]]);
+            break;
+        case 4:
+            await keyboard.releaseKey(Key[key[3]], Key[key[2]], Key[key[1]], Key[key[0]]);
+            break;
+    }
+}
+
+async function mykeyType(key, updown){
+    if(updown == 'down'){
+        await keyboard.type(key);
+        console.log('type: ' + key);
+    }
+}
+
+startWorker();
